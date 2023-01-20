0ab33ad7c25ab018a064c323153e4951604dd4d8
Chinnawat003 <Chinnawat1926@gmail.com>
2023-01-20 16:34:41 +0700

    first commit

---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,13 @@
+class Person constructor(val fname:String,age:Int) {
+    val firsName:String
+    val parage:Int
+
+    init {
+        firsName = fname
+        parage = age
+        println("First Name = $firsName")
+        println("First Name = $firsName Age = $age")
+    }
+}
+
+
